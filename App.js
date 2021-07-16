de1879aa7b713dab5de98c2c12a4796cefdeb0ee
andy <40680668+ajsmth@users.noreply.github.com>
2021-07-16 10:56:43 -0700

    camera permissions sdk-42

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera'
 
 export default function App() {
+
+  React.useEffect(() => {
+    Camera.requestCameraPermissionsAsync()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
