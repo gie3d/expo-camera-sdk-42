a0c2d735c519e5d07a02049dc622726639e3b2fb
gie <gie68i@gmail.com>
2021-07-20 09:27:12 +0700

    eject to bare workflow

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Camera } from 'expo-camera'
+import { Camera } from 'expo-camera';
 
 export default function App() {
+	React.useEffect(() => {
+		Camera.requestCameraPermissionsAsync();
+	}, []);
 
-  React.useEffect(() => {
-    Camera.requestCameraPermissionsAsync()
-  }, [])
-
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Camera
+				style={{ width: 300, height: 300 }}
+				type={Camera.Constants.Type.back}
+			/>
+			<Text>AAA</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'red',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
